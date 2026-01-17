7534d20cb0c86dea7f5b3af53f01207f01c4ecf3
Bibek Timilsina <bibektimilsina000@gmail.com>
2026-01-17 22:22:22 +0545

    fix: refactor UUID migration to use full batch mode for SQLite constraint support

--- fuse/alembic/versions/d98dd8ec85a3_edit_replace_id_integers_in_all_models_.py
+++ fuse/alembic/versions/d98dd8ec85a3_edit_replace_id_integers_in_all_models_.py
@@ -39,59 +39,43 @@ def upgrade():
         op.execute('UPDATE "user" SET new_id = uuid_generate_v4()')
         op.execute('UPDATE item SET new_id = uuid_generate_v4()')
     else:
-        # For SQLite/others, we might need a different approach if there's data
-        # But for fresh installs, it's fine. 
-        # For existing SQLite data, we'd need to generate UUIDs in Python
+        # For SQLite, if we have data, we'd need to generate UUIDs here.
+        # For new installs, the column is empty anyway.
         pass
 
     op.execute('UPDATE item SET new_owner_id = (SELECT new_id FROM "user" WHERE "user".id = item.owner_id)')
 
-    # Set the new_id as not nullable
+    # Set the new_id as not nullable and handle renaming/constraints in batch
     with op.batch_alter_table('user') as batch_op:
         batch_op.alter_column('new_id', nullable=False)
+        batch_op.drop_column('id')
+        batch_op.alter_column('new_id', new_column_name='id')
+        batch_op.create_primary_key('user_pkey', ['id'])
+
     with op.batch_alter_table('item') as batch_op:
         batch_op.alter_column('new_id', nullable=False)
-
-    # Drop old columns and rename new columns
-    if is_postgres:
-        op.drop_constraint('item_owner_id_fkey', 'item', type_='foreignkey')
-    
-    with op.batch_alter_table('item') as batch_op:
+        if is_postgres:
+            batch_op.drop_constraint('item_owner_id_fkey', type_='foreignkey')
         batch_op.drop_column('owner_id')
         batch_op.alter_column('new_owner_id', new_column_name='owner_id')
         batch_op.drop_column('id')
         batch_op.alter_column('new_id', new_column_name='id')
-
-    with op.batch_alter_table('user') as batch_op:
-        batch_op.drop_column('id')
-        batch_op.alter_column('new_id', new_column_name='id')
-
-    # Create primary key constraint
-    op.create_primary_key('user_pkey', 'user', ['id'])
-    op.create_primary_key('item_pkey', 'item', ['id'])
-
-    # Recreate foreign key constraint
-    op.create_foreign_key('item_owner_id_fkey', 'item', 'user', ['owner_id'], ['id'])
+        batch_op.create_primary_key('item_pkey', ['id'])
+        batch_op.create_foreign_key('item_owner_id_fkey', 'user', ['owner_id'], ['id'])
 
 def downgrade():
     bind = op.get_bind()
     is_postgres = bind.dialect.name == "postgresql"
 
-    # Drop new foreign key
-    if is_postgres:
-        op.drop_constraint('item_owner_id_fkey', 'item', type_='foreignkey')
-
-    # Reverse the upgrade process for item table
+    # Reverse the upgrade process
+    with op.batch_alter_table('user') as batch_op:
+        batch_op.add_column(sa.Column('old_id', sa.Integer, autoincrement=True))
+    
     with op.batch_alter_table('item') as batch_op:
         batch_op.add_column(sa.Column('old_id', sa.Integer, autoincrement=True))
         batch_op.add_column(sa.Column('old_owner_id', sa.Integer, nullable=True))
-    
-    # Reverse the upgrade process for user table
-    with op.batch_alter_table('user') as batch_op:
-        batch_op.add_column(sa.Column('old_id', sa.Integer, autoincrement=True))
 
     if is_postgres:
-        # Populate the old columns with default values (Postgres logic)
         op.execute('CREATE SEQUENCE IF NOT EXISTS user_id_seq AS INTEGER OWNED BY "user".old_id')
         op.execute('CREATE SEQUENCE IF NOT EXISTS item_id_seq AS INTEGER OWNED BY item.old_id')
         op.execute('SELECT setval(\'user_id_seq\', COALESCE((SELECT MAX(old_id) + 1 FROM "user"), 1), false)')
@@ -99,18 +83,17 @@ def downgrade():
         op.execute('UPDATE "user" SET old_id = nextval(\'user_id_seq\')')
         op.execute('UPDATE item SET old_id = nextval(\'item_id_seq\'), old_owner_id = (SELECT old_id FROM "user" WHERE "user".id = item.owner_id)')
     
-    # Drop new columns and rename old columns back
     with op.batch_alter_table('item') as batch_op:
+        if is_postgres:
+            batch_op.drop_constraint('item_owner_id_fkey', type_='foreignkey')
         batch_op.drop_column('owner_id')
         batch_op.alter_column('old_owner_id', new_column_name='owner_id')
         batch_op.drop_column('id')
         batch_op.alter_column('old_id', new_column_name='id')
+        batch_op.create_primary_key('item_pkey', ['id'])
+        batch_op.create_foreign_key('item_owner_id_fkey', 'user', ['owner_id'], ['id'])
 
     with op.batch_alter_table('user') as batch_op:
         batch_op.drop_column('id')
         batch_op.alter_column('old_id', new_column_name='id')
-
-    # Create constraints
-    op.create_primary_key('user_pkey', 'user', ['id'])
-    op.create_primary_key('item_pkey', 'item', ['id'])
-    op.create_foreign_key('item_owner_id_fkey', 'item', 'user', ['owner_id'], ['id'])
+        batch_op.create_primary_key('user_pkey', ['id'])
