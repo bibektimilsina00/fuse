130464b85e2765debfa30897a23875aee8aa778c
Bibek Timilsina <bibektimilsina000@gmail.com>
2026-01-19 17:28:17 +0545

    chore: bump version to 0.1.50 [skip ci]

--- fuse/cli.py
+++ fuse/cli.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 @click.group()
-@click.version_option(version="0.1.49")
+@click.version_option(version="0.1.50")
 def main():
     """
     Fuse - Workflow automation.
@@ -274,7 +274,7 @@ def version():
 
     table = Table(title="Version Information", show_header=False)
     table.add_row("Package", "fuse-io")
-    table.add_row("Version", "0.1.49")
+    table.add_row("Version", "0.1.50")
     table.add_row(
         "Python",
         f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}",
